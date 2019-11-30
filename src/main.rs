aabe4bdecc9f996486f426c6149b15aa5d128aae
TheKr1S <robert@wssns.nl>
2019-11-30 15:39:47 +0100

    generate small section large

--- src/main.rs
+++ src/main.rs
@@ -21,55 +21,6 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     None
 }
 
-use std::str::FromStr;
-
-/// Parse the string `s` as a coordinate pair, like `"400x600"` or `"1.0,0.5"`.
-///
-/// Specifically, `s` should have the form <left><sep><right>, where <sep> is
-/// the character given by the `separator` argument, and <left> and <right> are both
-/// strings that can be parsed by `T::from_str`.
-///
-/// If `s` has the proper form, return `Some<(x, y)>`. If it doesn't parse
-/// correctly, return `None`.
-fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
-    match s.find(separator) {
-        None => None,
-        Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
-                (Ok(l), Ok(r)) => Some((l, r)),
-                _ => None
-            }
-        }
-    }
-}
-
-#[test]
-fn test_parse_pair() {
-    assert_eq!(parse_pair::<i32>("",        ','), None);
-    assert_eq!(parse_pair::<i32>("10,",     ','), None);
-    assert_eq!(parse_pair::<i32>(",10",     ','), None);
-    assert_eq!(parse_pair::<i32>("10,20",   ','), Some((10, 20)));
-    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
-    assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
-    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
-}
-
-/// Parse a pair of floating-point numbers separated by a comma as a complex
-/// number.
-fn parse_complex(s: &str) -> Option<Complex<f64>> {
-    match parse_pair(s, ',') {
-        Some((re, im)) => Some(Complex { re, im }),
-        None => None
-    }
-}
-
-#[test]
-fn test_parse_complex() {
-    assert_eq!(parse_complex("1.25,-0.0625"),
-               Some(Complex { re: 1.25, im: -0.0625 }));
-    assert_eq!(parse_complex(",-0.0625"), None);
-}
-
 /// Given the row and column of a pixel in the output image, return the
 /// corresponding point on the complex plane.
 ///
@@ -121,7 +72,7 @@ fn render(pixels: &mut [u8],
             pixels[row * bounds.0 + column] =
                 match escape_time(point, 255) {
                     None => 0,
-                    Some(count) => 255 - count as u8
+                    Some(count) => (count % (128*2)) as u8  //255 - count as u8
                 };
         }
     }
@@ -133,7 +84,7 @@ use std::fs::File;
 
 /// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
 /// file named `filename`.
-fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
+fn write_image(filename: String, pixels: &[u8], bounds: (usize, usize))
     -> Result<(), std::io::Error>
 {
     let output = File::create(filename)?;
@@ -146,50 +97,58 @@ fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
     Ok(())
 }
 
-use std::io::Write;
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
-    if args.len() != 5 {
-        writeln!(std::io::stderr(),
-                 "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT")
-            .unwrap();
-        writeln!(std::io::stderr(),
-                 "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
-                 args[0])
-            .unwrap();
-        std::process::exit(1);
-    }
-
-    let bounds = parse_pair(&args[2], 'x')
-        .expect("error parsing image dimensions");
-    let upper_left = parse_complex(&args[3])
-        .expect("error parsing upper left corner point");
-    let lower_right = parse_complex(&args[4])
-        .expect("error parsing lower right corner point");
-
-    let mut pixels = vec![0; bounds.0 * bounds.1];
+fn generate_field(size: (usize, usize), pixels: &mut[u8], upper_left: Complex<f64>, lower_right: Complex<f64>) {
 
     // Scope of slicing up `pixels` into horizontal bands.
     {
         let bands: Vec<(usize, &mut [u8])> = pixels
-            .chunks_mut(bounds.0)
+            .chunks_mut(size.0)
             .enumerate()
             .collect();
-
         bands.into_par_iter()
             .weight_max()
             .for_each(|(i, band)| {
                 let top = i;
-                let band_bounds = (bounds.0, 1);
-                let band_upper_left = pixel_to_point(bounds, (0, top),
+                let band_bounds = (size.0, 1);
+                let band_upper_left = pixel_to_point(size, (0, top),
                                                      upper_left, lower_right);
-                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + 1),
+                let band_lower_right = pixel_to_point(size, (size.0, top + 1),
                                                       upper_left, lower_right);
                 render(band, band_bounds, band_upper_left, band_lower_right);
             });
     }
+    
+}
 
-    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+fn main() {
+
+    let field_size = (6400, 6400);
+    let mf_square_fields:usize = 10;
+
+    let mf_upper_left: Complex<f64> = Complex {re: -1.16, im: 0.29};
+    let mf_lower_right: Complex<f64> = Complex {re: -1.14, im: 0.275};
+    let mf_size = Complex{re: mf_upper_left.re - mf_lower_right.re, im: mf_upper_left.im - mf_lower_right.im};
+    let field_size_complex = Complex{re: mf_size.re / mf_square_fields as f64, im: mf_size.im / mf_square_fields as f64};
+
+    let mut pixels = vec![0; field_size.0 * field_size.1];
+
+
+    for row in 0..mf_square_fields {
+        for col in 0..mf_square_fields {
+            let re_offset = mf_size.re / mf_square_fields as f64 * col as f64;
+            let im_offset = mf_size.im / mf_square_fields as f64 * row as f64;
+            let field_upper_left = mf_upper_left - Complex{re: re_offset, im: im_offset};
+            let field_lower_right = field_upper_left - field_size_complex;
+
+            println!("generate {}_{} {:?} {:?}", row, col, field_upper_left, field_lower_right);
+
+            generate_field(field_size, &mut pixels, field_upper_left, field_lower_right);
+            
+            let filename = format!("field_{:03}_{:03}_0.png", row, col);
+            println!("Go write {}", filename);
+            write_image(filename, &pixels, field_size).expect("error writing PNG file");
+            println!("Write done");
+        }
+    }
 }
